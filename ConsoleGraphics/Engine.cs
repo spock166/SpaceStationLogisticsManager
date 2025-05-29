7d82a523fdd9ce12fb0fef5fcd5b42953eb035bd
spock166 <21161498+spock166@users.noreply.github.com>
2025-05-28 22:52:24 -0500

    Move graphics to seperate project

--- ConsoleGraphics/Engine.cs
+++ ConsoleGraphics/Engine.cs
@@ -1,12 +1,12 @@
-﻿namespace SpaceStationLogisticsManager
+﻿namespace ConsoleGraphics
 {
-    internal class ConsoleGraphics
+    public class Engine
     {
         private readonly char[,] buffer;
         private readonly int width;
         private readonly int height;
 
-        internal ConsoleGraphics(int consoleWidth, int consoleHeight)
+        public Engine(int consoleWidth, int consoleHeight)
         {
             Console.SetWindowSize(consoleWidth, consoleHeight);
             Console.SetBufferSize(consoleWidth, consoleHeight);
@@ -16,14 +16,14 @@
             Console.CursorVisible = false;
         }
 
-        internal void ClearBuffer()
+        public void ClearBuffer()
         {
             for (int y = 0; y < height; y++)
                 for (int x = 0; x < width; x++)
                     buffer[x, y] = ' ';
         }
 
-        internal void DrawStringToBuffer(int x, int y, string str)
+        public void DrawStringToBuffer(int x, int y, string str)
         {
             for (int index = 0; index < str.Length; index++)
             {
@@ -37,7 +37,7 @@
                 buffer[x, y] = c;
         }
 
-        internal void RenderBuffer()
+        public void RenderBuffer()
         {
             Console.SetCursorPosition(0, 0);
             for (int y = 0; y < height; y++)
--- SpaceStationLogisticsManager/Program.cs
+++ SpaceStationLogisticsManager/Program.cs
@@ -1,36 +1,51 @@
-﻿using DockingModule;
+﻿using ConsoleGraphics;
+using DockingModule;
 
 namespace SpaceStationLogisticsManager
 {
     public class Program
     {
         private static readonly Random rng = new Random();
+        private const int CONSOLE_WIDTH = 120;
+        private const int CONSOLE_HEIGHT = 40;
 
         public static void Main(string[] args)
         {
             bool running = true;
             int currentTick = 0;
             NavigationMap map = new NavigationMap(3, 3);
-            ConsoleGraphics graphics = new ConsoleGraphics(120, 40);
-
+            Engine graphics = new Engine(CONSOLE_WIDTH, CONSOLE_HEIGHT);
+            DrawBasicScreen(currentTick, graphics);
             while (running)
             {
-                graphics.ClearBuffer();
-                graphics.DrawStringToBuffer(0, 0, "Press Enter to advance time.");
-                graphics.DrawStringToBuffer(120 - 10, 0, "Tick " + currentTick);
-                graphics.DrawStringToBuffer(0, 1, "Hello World!");
-                graphics.RenderBuffer();
-
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo keyPressed = Console.ReadKey(true);
-                    if (keyPressed.Key == ConsoleKey.Enter)
+                    switch (keyPressed.Key)
                     {
-                        currentTick++;
+                        case ConsoleKey.Enter:
+                            currentTick++;
+                            break;
+                        case ConsoleKey.Escape:
+                            running = false;
+                            break;
+                        default:
+                            break;
                     }
+
+                    DrawBasicScreen(currentTick, graphics);
                 }
 
             }
+        }
+
+        private static void DrawBasicScreen(int currentTick, Engine engine)
+        {
+            engine.ClearBuffer();
+            engine.DrawStringToBuffer(0, 0, "Press Enter to advance time.");
+            engine.DrawStringToBuffer(CONSOLE_WIDTH - 10, 0, "Tick " + currentTick);
+            engine.DrawStringToBuffer(0, 1, "Hello World!");
+            engine.RenderBuffer();
         }
     }
 }
